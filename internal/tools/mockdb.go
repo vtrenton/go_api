6d49add850f2d0d3b9ba2b0d41f62686a05fbe51
Trent V <trenton.vanderwert@gmail.com>
2023-08-28 00:22:10 -0500

    added functions to mockDB

--- internal/tools/mockdb.go
+++ internal/tools/mockdb.go
@@ -1,1 +1,59 @@
 package tools
+
+import (
+	"time"
+)
+
+type mockDB struct{}
+
+var mockLoginDetails = map[string]LoginDetails{
+	"trent": {
+		AuthToken: "123ABC",
+		Username:  "trent",
+	},
+	"jason": {
+		AuthToken: "456DEF",
+		Username:  "jason",
+	},
+	"marie": {
+		AuthToken: "789GHI",
+		Username:  "marie",
+	},
+}
+
+var mockCoinDetails = map[string]CoinDetails{
+	"trent": {
+		Coins:    100,
+		Username: "trent",
+	},
+	"jason": {
+		Coins:    200,
+		Username: "jason",
+	},
+	"marie": {
+		Coins:    300,
+		Username: "marie",
+	},
+}
+
+func (d *mockDB) GetUserLoginDetails(username string) *LoginDetails {
+	// Simulate DB call
+	time.Sleep(time.Second * 1)
+	var clientData = LoginDetails{}
+	clientData, ok := mockLoginDetails[username]
+	if !ok {
+		return nil
+	}
+	return &clientData
+}
+
+func (d *mockDB) GetUserCoins(username string) *CoinDetails {
+	// simulate DB call
+	time.Sleep(time.Second * 1)
+	var clientData = CoinDetails{}
+	clientData, ok := mockCoinDetails[username]
+	if !ok {
+		return nil
+	}
+	return &clientData
+}
